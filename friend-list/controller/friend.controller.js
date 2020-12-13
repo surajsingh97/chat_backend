be51563a1aa22ee64f387c0ffeef65e809174e2c
surajsingh97 <suraj19bartwal97@gmail.com>
2020-12-13 19:32:47 +0530

    first

--- friend-list/controller/friend.controller.js
+++ friend-list/controller/friend.controller.js
@@ -3,17 +3,34 @@ const friendService = require('../service/friendservice');
 const jwt = require('jsonwebtoken');
 
 exports.func = (req,res) =>{
-    const userName = req.body.userName;
+    const personName = req.body.userName;
+    console.log(personName);
     const token = req.token;
     const payload = jwt.verify(token, 'secret_key');
-    const personId = payload.id._id;
-    console.log(personId,'person id')
-    console.log(userName);
-    friendService.findUser(userName).then((data,err)=>{
+    const userId = payload.id._id;
+    console.log(userId,'userId');
+    console.log(personName);
+    friendService.findUser(personName).then((data,err)=>{
         if(data){
-        
+            personId=data._id
+            const friendId = `${userId} ${personId}`
+            friendService.findselfbyId(userId).then((data,err)=>{
+                if(data){
+                    friendService.findAndUpdateFriendId(userId,friendId);
+                }else{
+                       const friendData = friend.create({userId: userId,friends:[{friendId: friendId}] });
+                       friendService.findfriendbyId(personId).then((data,err)=>{
+                           if(data){
+                               friendService.findAndUpdateFriendId(personId,friendId);
+                           }else{
+                               const anotherfriendData = friend.create({userId: personId,friends:[{friendId: friendId}] });
+                           }
+                       })
+                }
+            })
         }else{
             res.send('User Not found');
         }
     })
 }
+
--- friend-list/service/friendservice.js
+++ friend-list/service/friendservice.js
@@ -1,5 +1,5 @@
 const user = require('../../user/model/user.model');
-
+const friends = require('../model/friend.model');
 
 exports.findUser = (userName)=>{
     try{
@@ -7,5 +7,24 @@ exports.findUser = (userName)=>{
 
     }catch(err){
         console.log(err);
+    }
+}
+
+exports.findselfbyId = (id) =>{
+    try{
+        return friends.findOne({userId:id});
+    }catch(err){
+        console.log(err);
+    }
+}
+
+exports.findAndUpdateFriendId = (userId, friendId)=>{
+    try
+    {
+        return friends.findOneAndUpdate({ userId: userId},{$push: {friends: {friendId: friendId}}},{new : true});
+    }
+    catch(error)
+    {
+        throw error
     }
 }
